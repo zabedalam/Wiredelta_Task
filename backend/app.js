07abb7266fe334a5798cbd16072e676b3065dfbb
zabed <zabedalam777@gmail.com>
2021-08-04 12:42:51 +0200

    order APIS done

--- backend/app.js
+++ backend/app.js
@@ -10,11 +10,13 @@ app.use(cookieParser());
 //Import all routes
 const jobProposals = require("./routes/proposals");
 const auth = require("./routes/auth");
-const product= require('./routes/product')
+const product = require("./routes/product");
+const order = require("./routes/order");
 
 app.use("/api", jobProposals);
 app.use("/api", auth);
-app.use('/api', product);
+app.use("/api", product);
+app.use("/api", order);
 
 // Middleware to handle errors
 app.use(errorMiddleware);
