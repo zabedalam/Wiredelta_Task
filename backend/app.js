5ae63209f75ff368a0bb32df3c1f6ceda77f386a
zabed <zabedalam777@gmail.com>
2021-08-11 10:19:48 +0200

    added file uploader

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 const errorMiddleware = require("./middlewares/errors");
 const cors = require("cors");
 
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
+app.use(fileUpload());
+
 //Import all routes
 const jobProposals = require("./routes/proposals");
 const auth = require("./routes/auth");
