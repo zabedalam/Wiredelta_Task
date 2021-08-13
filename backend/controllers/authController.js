4dced842745c0bb249698457b24c13ceb36ac18f
zabed <zabedalam777@gmail.com>
2021-08-13 12:36:40 +0200

    some changes in controller for reset password working

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -68,9 +68,14 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   //Create reset password url
-  const resetUrl = `${req.protocol}://${req.get(
-    "host"
-  )}/api/password/reset/${resetToken}`;
+
+  // const resetUrl = `${req.protocol}://${req.get(
+  //   "host"
+  // )}/api/password/reset/${resetToken}`;
+
+  const resetUrl = `${process.env.FRONTEND_URL
+    
+  }/password/reset/${resetToken}`;
   const message = `Your password reset token is as follow:\n\n${resetUrl}\n\nIf you have not requested this email  then ignore it.`;
 
   try {
