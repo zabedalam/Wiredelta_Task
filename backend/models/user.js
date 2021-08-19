4fda11cdcec3543f2ebdaec7642db9a1fab88dae
zabed <zabedalam777@gmail.com>
2021-08-19 14:34:36 +0200

    state implemented for getting all orders for a logged in user

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const crypto=require('crypto')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please enter your name"],
-    maxLength: [30, "Your name cannot exceed 30 characters"]
+    maxLength: [30, "Your name cannot exceed 10 characters"]
   },
   email: {
     type: String,
@@ -22,16 +22,16 @@ const userSchema = new mongoose.Schema({
     minlength: [6, "Your password must be longer than 6 characters"],
     select: false
   },
-avatar:{
-  public_id:{
-    type:String,
-    required:true
+  avatar: {
+    public_id: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    }
   },
-  url:{
-    type:String,
-    required:true
-  }
-},
   role: {
     type: String,
     default: "user"
@@ -45,38 +45,41 @@ avatar:{
 });
 
 // Encrypting password before saving user
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        next()
-    }
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
 
-    this.password = await bcrypt.hash(this.password, 10)
-})
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
 // Compare user password
 userSchema.methods.comparePassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password)
-}
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 // Return JWT token
 userSchema.methods.getJwtToken = function () {
-    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_TIME
-    });
-}
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_TIME
+  });
+};
 
 //Generate password reset token
-userSchema.methods.getResetPasswordToken=function(){
+userSchema.methods.getResetPasswordToken = function () {
   //Generate token
-  const resetToken=crypto.randomBytes(20).toString('hex')
+  const resetToken = crypto.randomBytes(20).toString("hex");
 
   //Hash and set to resetPasswordToken
-  this.resetPasswordToken=crypto.createHash('sha256').update(resetToken).digest('hex')
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
 
   //Set token expire time
-  this.resetPasswordExpire=Date.now()+30*60*1000
+  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
 
-  return resetToken
-}
+  return resetToken;
+};
 
 module.exports = mongoose.model("User", userSchema);
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,7 +11,7 @@ import {
   forgotPasswordReducer
 } from "./reducers/userReducers";
 import { cartReducer } from "./reducers/cartReducers";
-import { newOrderReducer } from "./reducers/orderReducers";
+import { newOrderReducer, myOrdersReducer } from "./reducers/orderReducers";
 
 const reducer = combineReducers({
   products: productsReducer,
@@ -20,7 +20,8 @@ const reducer = combineReducers({
   user: userReducer,
   forgotPassword: forgotPasswordReducer,
   cart: cartReducer,
-  newOrder: newOrderReducer
+  newOrder: newOrderReducer,
+  myOrders: myOrdersReducer
 });
 
 let initialState = {
